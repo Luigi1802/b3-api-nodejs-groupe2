ff5519e00cfe7953df389751a16034a2503639c9
issam <issame.hassane@hotmail.com>
2024-07-23 11:40:34 +0200

    wip

--- backend/swagger.js
+++ backend/swagger.js
@@ -11,8 +11,5 @@ const doc = {
 const outputFile = './swagger.json';
 const routes = ['./src/routes/movieRoutes.js'];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
-
 
 swaggerAutogen({openapi: '3.1.0'})(outputFile, routes, doc);
