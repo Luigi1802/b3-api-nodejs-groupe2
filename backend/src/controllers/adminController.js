a145b0a51b6183c8761716d6d5dbf4d30392c4d3
Luigi <luigiaubrypouget@gmail.com>
2024-07-23 15:14:31 +0200

    feat: ajout routes et controllers pour /admin/users/getOne et /admin/users/getAll

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,4 +1,5 @@
 import Movie from "../models/movieModel.js";
+import User from "../models/userModel.js";
 
 const movieAdd = async (request, response, next)=>{
     try {
@@ -54,4 +55,36 @@ const movieDelete = async (request, response, next)=>{
     }
 }
 
-export {movieAdd, movieUpdate, movieDelete};
+const userGetOne = async (request, response, next)=>{
+    const userId = request.query.id;
+
+    if (!userId) {
+        return response.status(400).json({message:"User id is required."})
+    }
+
+    try {
+        const result = await User.find({_id:userId});
+        if (result.length > 0) {
+            return response.status(200).send(result);
+        } else {
+            return response.status(204).send(result);
+        }
+    }catch (err){
+        return response.status(500).json({message:"Unexpected error, please contact an admnistrator."})
+    }
+}
+
+const userGetAll = async (request, response, next)=>{
+    try {
+        const result = await User.find({});
+        if (result.length > 0) {
+            return response.status(200).send(result);
+        } else {
+            return response.status(204).send(result);
+        }
+    }catch (err){
+        return response.status(500).json({message:"Unexpected error, please contact an admnistrator."})
+    }
+}
+
+export {movieAdd, movieUpdate, movieDelete, userGetOne, userGetAll};
--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import adminAuth from "../controllers/adminAuth.js";
-import {movieAdd, movieUpdate, movieDelete} from "../controllers/adminController.js";
+import {movieAdd, movieUpdate, movieDelete, userGetAll, userGetOne} from "../controllers/adminController.js";
 import checkTokenValid from "../middlewares/validationToken.js";
 
 const router = express.Router();
@@ -10,5 +10,8 @@ router.post('/admin/login', adminAuth);
 router.post('/admin/movies/', checkTokenValid, movieAdd);
 router.patch('/admin/movies/', checkTokenValid, movieUpdate);
 router.delete('/admin/movies/', checkTokenValid, movieDelete);
+
+router.get('/admin/users/getAll', checkTokenValid, userGetAll);
+router.get('/admin/users/getOne', checkTokenValid, userGetOne);
 
 export default router;
