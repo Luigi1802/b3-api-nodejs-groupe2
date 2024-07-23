e20367b331681f1e4bed70ffec8e3661d07f74a5
pho-rg <phdorglandes@hotmail.fr>
2024-07-23 12:56:35 +0200

    refactor: user model

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,57 +1,50 @@
 import User from "../models/userModel.js";
 
 const userPatch = async (request, response, next) => {
-    /*let user: any;
-
-    const _id = request.body._id;
-
-    await User.findOne({_id: _id}).then((results) => {
-        if (results) {
-            user = results;
-        } else {
-            return response.status(404).json("Aucun utilisateur trouvé avec l'id envoyé.")
+    try {
+        const userId = request.query.id;
+        const userData = request.body;
+        const updatedUser = await User.findByIdAndUpdate(userId, userData, {new: true});
+        if (!updatedUser) {
+            response.status(404).send(`User ${userId} not found.`)
         }
-    }).catch((err) => {
-        return response.status(400).send(err.message);
-    });
-
-    if (user) {
-        user.email = request.body.email;
-        user.password = request.body.password;
-        user.name = request.body.name;
-        user.surname = request.body.surname;
-    } else {
+        return response.status(200).send(updatedUser);
+    } catch (err) {
+        if (err.name === "CastError") {
+            return response.status(400).send(err.message);
+        }
         return response.status(500).send("Unexpected error, please contact an admnistrator.");
     }
-
-    try {
-        await user.save()
-        return response.status(201).json({message: "Utilisateur mis à jour avec succès."});
-    } catch (err){
-        return response.status(500).send("Unexpected error, please contact an admnistrator.");
-    }*/
 }
 
 const userPostFavorites = async (request, response, next) => {
     try {
         // pass
-    } catch (err){
+    } catch (err) {
         return response.status(500).send("Unexpected error, please contact an admnistrator.")
     }
 }
 
 const userGetAllFavorites = async (request, response, next) => {
     try {
-        // pass
-    } catch (err){
-        return response.status(500).send("Unexpected error, please contact an admnistrator.")
+        const userId = request.query.id;
+        const user = await User.findById(userId).select('favorites');
+        if (!user) {
+            return response.status(404).send(`User ${userId} not found.`);
+        }
+        return response.status(200).send(user.favorites);
+    } catch (err) {
+        if (err.name === "CastError") {
+            return response.status(400).send(err.message);
+        }
+        return response.status(500).send("Unexpected error, please contact an administrator.");
     }
 }
 
 const userDeleteFavorites = async (request, response, next) => {
     try {
         // pass
-    } catch (err){
+    } catch (err) {
         return response.status(500).send("Unexpected error, please contact an admnistrator.")
     }
 }
@@ -59,7 +52,7 @@ const userDeleteFavorites = async (request, response, next) => {
 const userPostWatchlist = async (request, response, next) => {
     try {
         // pass
-    } catch (err){
+    } catch (err) {
         return response.status(500).send("Unexpected error, please contact an admnistrator.")
     }
 }
@@ -67,7 +60,7 @@ const userPostWatchlist = async (request, response, next) => {
 const userGetAllHistory = async (request, response, next) => {
     try {
         // pass
-    } catch (err){
+    } catch (err) {
         return response.status(500).send("Unexpected error, please contact an admnistrator.")
     }
 }
@@ -75,10 +68,18 @@ const userGetAllHistory = async (request, response, next) => {
 const userPatchDeleteAccount = async (request, response, next) => {
     try {
         // pass
-    } catch (err){
+    } catch (err) {
         return response.status(500).send("Unexpected error, please contact an admnistrator.")
     }
 }
 
 
-export {userPatch, userPostFavorites, userGetAllFavorites, userDeleteFavorites, userPostWatchlist, userGetAllHistory, userPatchDeleteAccount};
+export {
+    userPatch,
+    userPostFavorites,
+    userGetAllFavorites,
+    userDeleteFavorites,
+    userPostWatchlist,
+    userGetAllHistory,
+    userPatchDeleteAccount
+};
