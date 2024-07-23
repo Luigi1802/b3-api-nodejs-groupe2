d0e2d08c99d240a31f19d299b86b7c1a3935c500
pho-rg <phdorglandes@hotmail.fr>
2024-07-23 16:01:02 +0200

    feat: ajout des routes user, patch et get ok - post wip

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,23 +1,17 @@
 import express from "express";
-import {userPatch,
-    userPostFavorites,
-    userGetAllFavorites,
-    userDeleteFavorites,
-    userPostWatchlist,
-    userGetAllHistory,
-    userPatchDeleteAccount} from "../controllers/userController.js";
+import {userPatch, userPostFavorites, userGetAllFavorites, userDeleteFavorites, userPostWatchlist, userGetAllHistory, userPatchDeleteAccount} from "../controllers/userController.js";
 import checkTokenValid from "../middlewares/validationToken.js";
 import userAuth from "../controllers/userAuth.js";
 const userRouter = express.Router();
 
 userRouter.post('/user/login', userAuth)
 
-userRouter.patch('/user/',checkTokenValid, userPatch);
-userRouter.post('/user/favorites/', userPostFavorites);
-userRouter.get('/user/favorites/', userGetAllFavorites);
-userRouter.delete('/user/favorites/', userDeleteFavorites);
-userRouter.post('/user/watchlist/', userPostWatchlist);
-userRouter.get('/user/history',checkTokenValid, userGetAllHistory);
-userRouter.patch('/user/deleteAccount/', userPatchDeleteAccount)
+userRouter.patch('/user/', checkTokenValid, userPatch);
+userRouter.post('/user/favorites/', checkTokenValid, userPostFavorites);
+userRouter.get('/user/favorites/', checkTokenValid, userGetAllFavorites);
+userRouter.delete('/user/favorites/', checkTokenValid, userDeleteFavorites);
+userRouter.post('/user/watchlist/', checkTokenValid, userPostWatchlist);
+userRouter.get('/user/history', checkTokenValid, userGetAllHistory);
+userRouter.patch('/user/deleteAccount/', checkTokenValid, userPatchDeleteAccount);
 
 export default userRouter;
